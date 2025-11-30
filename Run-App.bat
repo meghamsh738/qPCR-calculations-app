a3f863b1050f5fca0aeacf92a5c47a9ba9ce5a1b
Megha <megha@bio172037.college.tcd.ie>
2025-11-30 10:33:47 +0000

    Use relative WSL path in launcher

--- Run-App.bat
+++ Run-App.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
-set "WSL_PATH='<PROJECTS_DIR>/qpcr-calculations-app-git/modern-app'"
-start "qPCR calc servers" wsl -e bash -lc "cd %WSL_PATH% && npm run dev:full"
+for /f "usebackq tokens=*" %%i in (`wsl wslpath "%~dp0"`) do set WSL_DIR=%%i
+start "qPCR calc servers" wsl -e bash -lc "cd \"%WSL_DIR%modern-app\" && npm run dev:full"
 timeout /t 4 >nul
 start "" http://localhost:5176
 endlocal
